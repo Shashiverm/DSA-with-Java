1b808f2c229d3c96e2e26b1eba48c1110e55dd9c
Shashiverm <meetshashikantkumar@gmail.com>
2026-09-05 08:49:31 +0530

    05-09-2026

---
+++ leetcode/05_09_2026/Solution.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+        int n = nums.length;
+
+        // suffix minimum
+        int[] suffMin = new int[n];
+        suffMin[n - 1] = nums[n - 1];
+
+        for (int i = n - 2; i >= 0; i--) {
+            suffMin[i] = Math.min(nums[i], suffMin[i + 1]);
+        }
+
+        // prefix maximum + find first stable index
+        int prefMax = nums[0];
+
+        for (int i = 0; i < n; i++) {
+            prefMax = Math.max(prefMax, nums[i]);
+
+            int instability = prefMax - suffMin[i];
+
+            if (instability <= k) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
